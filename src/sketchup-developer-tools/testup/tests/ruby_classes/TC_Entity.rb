81975dd4bd6f306fad93572458924a0e609de1de
Aerilius <aerilius@gmail.com>
2012-12-07 16:23:35 +0100

    #37: ajust variable names • renamed `edge` into "cline` / `text` • duplicate method `test_set_attribute_float`. An integer test was missing so I replaced it by `test_set_attribute_int`.

--- src/sketchup-developer-tools/testup/tests/ruby_classes/TC_Entity.rb
+++ src/sketchup-developer-tools/testup/tests/ruby_classes/TC_Entity.rb
@@ -270,8 +270,8 @@ class TC_Entity < Test::Unit::TestCase
   def test_to_s_cline
     erase_all()
     model = Sketchup.active_model
-    edge = model.entities.add_cline([0,0,0], [100,100,100])
-    assert_equal(edge.to_s.index('Sketchup::ConstructionLine'), 2,
+    cline = model.entities.add_cline([0,0,0], [100,100,100])
+    assert_equal(cline.to_s.index('Sketchup::ConstructionLine'), 2,
                  'Failed in test_to_s_cline')
   end
 
@@ -279,8 +279,8 @@ class TC_Entity < Test::Unit::TestCase
   def test_to_s_text
     erase_all()
     model = Sketchup.active_model
-    edge = model.entities.add_text "This is a Test", [0,0,0]
-    assert_equal(edge.to_s.index('Sketchup::Text'), 2,
+    text = model.entities.add_text "This is a Test", [0,0,0]
+    assert_equal(text.to_s.index('Sketchup::Text'), 2,
                  'Failed in test_to_s_text')
   end
 
@@ -334,17 +334,17 @@ class TC_Entity < Test::Unit::TestCase
   def test_typename_cline
     erase_all()
     model = Sketchup.active_model
-    edge = model.entities.add_cline([0,0,0], [100,100,100])
-    assert_equal(edge.typename.index('Sketchup::ConstructionLine'),
-                 'ConstructionLine', 'Failed in test_typename_cline')
+    cline = model.entities.add_cline([0,0,0], [100,100,100])
+    assert_equal(cline.typename, 'ConstructionLine',
+                 'Failed in test_typename_cline')
   end
 
   # Test that the edge returns the expected type.
   def test_typename_text
     erase_all()
     model = Sketchup.active_model
-    edge = model.entities.add_text "This is a Test", [0,0,0]
-    assert_equal(edge.typename, 'Text',
+    text = model.entities.add_text "This is a Test", [0,0,0]
+    assert_equal(text.typename, 'Text',
                  'Failed in test_typename_text')
   end
 
@@ -774,13 +774,13 @@ class TC_Entity < Test::Unit::TestCase
                  'Failed in test_set_attribute_string')
   end
 
-  def test_set_attribute_float
+  def test_set_attribute_int
     model = Sketchup.active_model
     entities = model.active_entities
     edge = entities.add_line([0,0,0],[9,9,9])
-    status = edge.set_attribute "testdictionary", "test", 5.5
-    assert_equal(status, 5.5,
-                 'Failed in test_set_attribute_float')
+    status = edge.set_attribute "testdictionary", "test", 5
+    assert_equal(status, 5,
+                 'Failed in test_set_attribute_int')
   end
 
   def test_set_attribute_float
